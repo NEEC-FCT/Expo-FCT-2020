41fb67062c0d6957b078d41e694e7c28501c1bf2
João Veloso <jm.veloso@campus.fct.unl.pt>
2020-02-19 14:37:20 +0000

    Base code

---
+++ expo_fct_2020/android/app/src/main/kotlin/fct/neec/expo_fct_2020/MainActivity.kt
@@ -0,0 +1,12 @@
+package fct.neec.expo_fct_2020
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
