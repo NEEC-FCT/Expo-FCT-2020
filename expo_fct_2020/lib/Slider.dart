088ea02f595c72177cd678c497bb1623c97a7e17
João Veloso <jm.veloso@campus.fct.unl.pt>
2020-02-21 18:56:42 +0000

    Bug do Slider corrigido

--- expo_fct_2020/lib/Slider.dart
+++ expo_fct_2020/lib/Slider.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:expo_fct_2020/main.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Animation_Gesture/page_dragger.dart';
 import 'Animation_Gesture/page_reveal.dart';
@@ -34,29 +36,52 @@ class _SlidePageState extends State<SlidePage> with TickerProviderStateMixin {
   AnimatedPageDragger animatedPageDragger;
 
   int activeIndex = 0 ;
+  int endOfslider = 3;
   SlideDirection slideDirection = SlideDirection.none;
   int nextPageIndex = 0 ;
   double slidePercent= 0.0;
+
+  void onDonePress() async {
+    // Do what you want
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("slider", true);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyApp()),
+    );
+  }
 
   _SlidePageState(){
     slideUpdateStream = new StreamController<SlideUpdate>();
 
     slideUpdateStream.stream.listen((SlideUpdate event){
-      setState(() {
-        if( event.updateType == UpdateType.dragging){
+      setState(()  {
+        if (nextPageIndex == endOfslider) {
+          nextPageIndex--;
+          print("Mostra o menu");
+          onDonePress();
+
+        }
+        else{
+
+        if (event.updateType == UpdateType.dragging) {
           slideDirection = event.direction;
           slidePercent = event.slidePercent;
 
-          if( slideDirection == SlideDirection.leftToRight ){
+          print(nextPageIndex);
+
+
+          if (slideDirection == SlideDirection.leftToRight) {
             nextPageIndex = activeIndex - 1;
-          } else if (slideDirection == SlideDirection.rightToLeft){
+          } else if (slideDirection == SlideDirection.rightToLeft) {
             nextPageIndex = activeIndex + 1;
-          } else{
+          }
+          else {
             nextPageIndex = activeIndex;
           }
-        } else if( event.updateType == UpdateType.doneDragging){
-          if(slidePercent > 0.5){
-
+        } else if (event.updateType == UpdateType.doneDragging) {
+          if (slidePercent > 0.5) {
             animatedPageDragger = new AnimatedPageDragger(
               slideDirection: slideDirection,
               transitionGoal: TransitionGoal.open,
@@ -64,8 +89,7 @@ class _SlidePageState extends State<SlidePage> with TickerProviderStateMixin {
               slideUpdateStream: slideUpdateStream,
               vsync: this,
             );
-
-          } else{
+          } else {
             animatedPageDragger = new AnimatedPageDragger(
               slideDirection: slideDirection,
               transitionGoal: TransitionGoal.close,
@@ -79,12 +103,12 @@ class _SlidePageState extends State<SlidePage> with TickerProviderStateMixin {
 
           animatedPageDragger.run();
         }
-        else if( event.updateType == UpdateType.animating){
+        else if (event.updateType == UpdateType.animating) {
           slideDirection = event.direction;
           slidePercent = event.slidePercent;
         }
 
-        else if (event.updateType == UpdateType.doneAnimating){
+        else if (event.updateType == UpdateType.doneAnimating) {
           activeIndex = nextPageIndex;
 
           slideDirection = SlideDirection.none;
@@ -92,6 +116,7 @@ class _SlidePageState extends State<SlidePage> with TickerProviderStateMixin {
 
           animatedPageDragger.dispose();
         }
+      }
       });
     });
 
@@ -124,7 +149,7 @@ class _SlidePageState extends State<SlidePage> with TickerProviderStateMixin {
           ),
           new PageDragger(
             canDragLeftToRight: activeIndex > 0 ,
-            canDragRightToLeft: activeIndex < pages.length - 1 ,
+            canDragRightToLeft: activeIndex < pages.length  ,
             slideUpdateStream: this.slideUpdateStream,
           )
         ],
--- expo_fct_2020/lib/main.dart
+++ expo_fct_2020/lib/main.dart
@@ -9,6 +9,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -77,6 +79,9 @@ class _MyHomePageState extends State<MyHomePage> {
     SystemChrome.setEnabledSystemUIOverlays([]);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+
+
 
     return Scaffold(
       body: Center(
