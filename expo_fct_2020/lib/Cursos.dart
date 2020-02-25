3c06a5f949c4766afd0fc67165f346af1cf496a2
João Veloso <jm.veloso@campus.fct.unl.pt>
2020-02-25 01:00:46 +0000

    URL curso abre no browser

--- expo_fct_2020/lib/Cursos.dart
+++ expo_fct_2020/lib/Cursos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Cursos extends StatefulWidget {
@@ -31,10 +32,24 @@ class _CursosState extends State<Cursos> {
                 child: WebView(
               initialUrl: 'https://expofct.neec-fct.com/cursos/',
               javascriptMode: JavascriptMode.unrestricted,
+                    navigationDelegate: (NavigationRequest request) {
+                      print(request.url.toString());
+                      _launchURL(request.url);
+                      return NavigationDecision.prevent;
+                    }
             )),
           ],
         ),
       ),
     );
+  }
+
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
